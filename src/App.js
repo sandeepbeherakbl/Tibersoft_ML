3b89037ab201ba2f765f45b6bb0f29df6ad978a4
sandeepbeherakbl <sandeepbeherakbl@gmail.com>
2024-03-21 12:45:30 +0530

    test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,20 @@ import MapComponent from "./components/mappage";
 function App() {
   return (
     <>
-      <MapComponent />
+      {/* <MapComponent /> */}
+      <a href="BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//hacksw/handcal//NONSGML v1.0//EN
+BEGIN:VEVENT
+UID:1234567890
+DTSTAMP:20240101T120000
+DTSTART:20240115T090000
+DTEND:20240115T100000
+SUMMARY:Test Event
+LOCATION:Test Location
+DESCRIPTION:This is a test event.
+END:VEVENT
+END:VCALENDAR" download="event.ics">Add to Calendar</a>
     </>
   );
 }
